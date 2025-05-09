db2fc718194b2cf048cac26c1cff4c70d0f603ae
dem4ron <demaaron88@gmail.com>
2025-05-09 17:25:22 +0200

    Comment out unused stuff

--- src/clone-node.ts
+++ src/clone-node.ts
@@ -1,11 +1,6 @@
 import type { Options } from './types'
 import { clonePseudoElements } from './clone-pseudos'
-import {
-  createImage,
-  toArray,
-  isInstanceOfElement,
-  getStyleProperties,
-} from './util'
+import { createImage, toArray, isInstanceOfElement } from './util'
 import { getMimeType } from './mimes'
 import { resourceToDataURL } from './dataurl'
 
@@ -119,49 +114,49 @@ async function cloneChildren<T extends HTMLElement>(
   return clonedNode
 }
 
-function cloneCSSStyle<T extends HTMLElement>(
-  nativeNode: T,
-  clonedNode: T,
-  options: Options,
-) {
-  const targetStyle = clonedNode.style
-  if (!targetStyle) {
-    return
-  }
+// function cloneCSSStyle<T extends HTMLElement>(
+//   nativeNode: T,
+//   clonedNode: T,
+//   options: Options,
+// ) {
+//   const targetStyle = clonedNode.style
+//   if (!targetStyle) {
+//     return
+//   }
 
-  const sourceStyle = window.getComputedStyle(nativeNode)
-  if (sourceStyle.cssText) {
-    targetStyle.cssText = sourceStyle.cssText
-    targetStyle.transformOrigin = sourceStyle.transformOrigin
-  } else {
-    getStyleProperties(options).forEach((name) => {
-      let value = sourceStyle.getPropertyValue(name)
-      if (name === 'font-size' && value.endsWith('px')) {
-        const reducedFont =
-          Math.floor(parseFloat(value.substring(0, value.length - 2))) - 0.1
-        value = `${reducedFont}px`
-      }
+//   const sourceStyle = window.getComputedStyle(nativeNode)
+//   if (sourceStyle.cssText) {
+//     targetStyle.cssText = sourceStyle.cssText
+//     targetStyle.transformOrigin = sourceStyle.transformOrigin
+//   } else {
+//     getStyleProperties(options).forEach((name) => {
+//       let value = sourceStyle.getPropertyValue(name)
+//       if (name === 'font-size' && value.endsWith('px')) {
+//         const reducedFont =
+//           Math.floor(parseFloat(value.substring(0, value.length - 2))) - 0.1
+//         value = `${reducedFont}px`
+//       }
 
-      if (
-        isInstanceOfElement(nativeNode, HTMLIFrameElement) &&
-        name === 'display' &&
-        value === 'inline'
-      ) {
-        value = 'block'
-      }
+//       if (
+//         isInstanceOfElement(nativeNode, HTMLIFrameElement) &&
+//         name === 'display' &&
+//         value === 'inline'
+//       ) {
+//         value = 'block'
+//       }
 
-      if (name === 'd' && clonedNode.getAttribute('d')) {
-        value = `path(${clonedNode.getAttribute('d')})`
-      }
+//       if (name === 'd' && clonedNode.getAttribute('d')) {
+//         value = `path(${clonedNode.getAttribute('d')})`
+//       }
 
-      targetStyle.setProperty(
-        name,
-        value,
-        sourceStyle.getPropertyPriority(name),
-      )
-    })
-  }
-}
+//       targetStyle.setProperty(
+//         name,
+//         value,
+//         sourceStyle.getPropertyPriority(name),
+//       )
+//     })
+//   }
+// }
 
 function cloneInputValue<T extends HTMLElement>(nativeNode: T, clonedNode: T) {
   if (isInstanceOfElement(nativeNode, HTMLTextAreaElement)) {
